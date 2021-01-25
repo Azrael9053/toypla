3399bef64ae416474ba0c1e757a7f91ee0493e8c
Azrael9053 <jkss9053hrf@gmail.com>
2021-01-25 16:28:20 +0800

    sun

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ $(function() {
         $(this).children("ul").stop().slideToggle(100)
     });
 });
+
+$(function() {
+    var headerPos = $("header").offset().top;
+    $(window).scroll(function() {
+        if($(window).scrollTop() > headerPos){
+            $("header").css("position", "fixed");
+        }
+        else{
+            $("header").css("position", "static");
+        }
+    });
+});
+
+$(function() {
+    $("a[href*=#]:not([href=#])").click(function() {
+        var target = $($(this).attr("href")).offset().top;
+        target -= 110;
+
+        $("html, body").animate({scrollTop: target}, 500);
+
+        return false;
+    })
+})
