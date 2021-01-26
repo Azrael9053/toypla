77d2cc9e06f149f0272172432373b3e972dd5b94
Azrael9053 <jkss9053hrf@gmail.com>
2021-01-27 02:36:48 +0800

    background&flex

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ $(function() {
     $(window).scroll(function() {
         if($(window).scrollTop() > headerPos){
             $("header").css("position", "fixed");
+            $("body").addClass("is-sticky");
         }
         else{
             $("header").css("position", "static");
+            $("body").removeClass("is-sticky");
         }
     });
 });
@@ -19,7 +21,7 @@ $(function() {
 $(function() {
     $("a[href*=#]:not([href=#])").click(function() {
         var target = $($(this).attr("href")).offset().top;
-        target -= 110;
+        target -= 85;
 
         $("html, body").animate({scrollTop: target}, 500);
 
